6542ddf5931cbaf50579108255b607a96355967d
DanielCyberSec <166703297+DanielCyberSec@users.noreply.github.com>
2024-04-17 18:45:20 -0400

    Add files via upload

--- dispositivosEnRed.sh
+++ dispositivosEnRed.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 #https://github.com/DanielCyberSec/ScanningTools.git
-
-#importamos el scrips
-#. $miIdRed+CIDR.sh
-#. $miIP.sh
-#Validamos permisos
 rojoNegrita="\e[1;31m"
 verde="\e[32m"
 sinColor="\e[0m"
-
+amNeg="\e[1;33m"
+echo -e "${amNeg}######################################"
+echo -e "##        ScanningTools             ##"
+echo -e "##        by DanielCyberSec         ##"
+echo -e "######################################$sinColor"
+#Validamos permisos
 if [ $(id -u) -ne 0 ]; then
         echo -e "${rojoNegrita} Se requieren permisos elevados...${sinColor}"
 exit
@@ -17,8 +17,6 @@ fi
 echo "Comprobando que exista nmap en el equipo:"
 if [ "$(echo $?)" == "0" ]; then
         echo -e "$(nmap --version | head -n 1 | awk '{print $1, $2, $3}') ............ [${verde} OK ${sinColor}]"
-        sleep 3
-        #clear
 	idRedCIDR=$(./miIdRed+CIDR.sh)
 	miIP=$(./miIP.sh)
 	echo Mi IP: $miIP
